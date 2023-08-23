5f98a90e74e660d945430fb7e1feebafb426c654
Agustin Carretto <etemagus@gmail.com>
2023-08-23 15:39:30 -0300

    agregado salto, solucionado keysQueue

--- scripts.js
+++ scripts.js
@@ -5,10 +5,16 @@ const CANVAS_HEIGHT = canvas.height = 600;
 const PLATER_WIDTH = 100;
 const PLATER_HEIGHT = 100;
 const playerImage = new Image();
+const keysQueue= [];
+
+
 
 let posX = 0;
 let posY = 0;
 let velocity = 0;
+let gravity = 3;
+let accelY = 0;
+let isJumping = false;
 
 ctx.imageSmoothingEnabled = true;
 
@@ -23,9 +29,19 @@ playerImage.src = 'Imagenes/run/run_0.png';
 function animate() {
 
     posX += velocity;
+    if (posY > 1){
+        posY = Math.max(0,posY + accelY);
+        accelY = accelY - gravity;
+        console.log(posY,accelY);
+    }
+    else{
+        posY = 0;
+        accelY = 0;
+        isJumping = false;
+    }
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(playerImage, posX, CANVAS_HEIGHT-PLATER_HEIGHT, PLATER_WIDTH, PLATER_HEIGHT);
-    frame = Math.floor(requestAnimationFrame(animate)/4);
+    ctx.drawImage(playerImage, posX, CANVAS_HEIGHT-PLATER_HEIGHT - posY, PLATER_WIDTH, PLATER_HEIGHT);
+    frame = Math.floor(requestAnimationFrame(animate)/1,5);
     playerImage.src = 'Imagenes/'+currentState+'/'+ currentState +'_'+ frame % states[currentState] + '.png';
 }
 
@@ -37,13 +53,27 @@ function moveLeft(){
     Math.abs(velocity) > 3 ? velocity -= 0 : velocity -= 4;
 }
 
+function jump(){
+    if (!isJumping){
+        posY = 5;
+        accelY = 40;
+        isJumping = true;
+    }
+}
+
 function moveRight(){
     Math.abs(velocity) > 3 ? velocity += 0 : velocity += 4;
 }
 
 document.addEventListener('keydown', function(event){
+    if (!keysQueue.includes(event.key)){
+        keysQueue.push(event.key);
+    }
     console.log(event.key)
     if (event.key == 'a'){
+        if (keysQueue.includes(event.key)){
+            
+        }
         setState('run');
         moveLeft();
         if (playerImage.dataset.direction != 'left'){
@@ -60,11 +90,20 @@ document.addEventListener('keydown', function(event){
             playerImage.transform = 'scaleX(1)';
         }
     }
+
+    if (event.key == 'w'){
+    setState('run');
+    jump();
+    }
+
 })
 
 document.addEventListener('keyup', function(event){
-    setState('idle');
-    velocity = 0;
+    keysQueue.splice(keysQueue.indexOf(event.key),1);
+    if (keysQueue.length == 0){
+        setState('idle');
+        velocity = 0;
+    }
 })
 
 
